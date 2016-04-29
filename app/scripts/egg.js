010aa907ef086ae1bf0da2e2a5e0f7d28ebde11a
MarcusG <the.macon7@gmail.com>
2016-04-29 07:21:21 +0200

    + added a much cooler base for the egg guy!

--- app/scripts/egg.js
+++ app/scripts/egg.js
@@ -13,6 +13,9 @@ var colors = [
 
 for (var row = rows; row > 0; row--) {
   for (var col = cols; col > 0; col--) {
-    $('#dancefloor-main').append('<div class="cube ' + colors[row % 2][col % 2] + '"></div>')
+    $('#dancefloor-main').append('<div class="cube ' + colors[row % 2][col % 2] + '"></div>');
   }
+
+  $('#dancefloor-left').append('<div class="cube ' + colors[1][row % 2] + '"></div>');
+  $('#dancefloor-right').append('<div class="cube ' + colors[row % 2][1] + '"></div>');
 }
